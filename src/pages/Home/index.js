4cc004b835f7775f05093351e306ce0a9a1ec92d
duncaiyun <759830766@qq.com>
2024-08-29 15:54:52 +0800

    chart组件封装

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,20 @@
-const Home = () =>{
-  return <div>this is home</div>
+import { BarChart } from './BarChart'
+
+const Home = () => {
+  return (
+    <div>
+      <BarChart
+        xData={['Vue', 'React', 'Angular']}
+        sData={[2000, 5000, 1000]} 
+        title='我的标题1'/>
+
+      <BarChart
+        xData={['Vue', 'React', 'Angular']}
+        sData={[200, 500, 100]}
+        style={{ width: '500px', height: '400px' }}
+        title='我的标题2' />
+    </div >
+  )
 }
+
 export default Home
