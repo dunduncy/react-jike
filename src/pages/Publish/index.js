70611f4f55ef07dbe95c725c123ccc3ff15ee4fa
duncaiyun <759830766@qq.com>
2024-08-29 16:45:12 +0800

    发布文章-频道列表的获取渲染

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -14,9 +14,22 @@ import { Link } from 'react-router-dom'
 import './index.scss'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
+import { useEffect, useState } from 'react'
+import { getChannelAPI } from '@/apis/article'
 const { Option } = Select
 
 const Publish = () => {
+  // 获取频道列表
+  const [channelList, setChannelList] = useState([])
+  useEffect(() => {
+    // 1. 封装函数 在函数体内调用接口
+    const getChannelList = async () => {
+      const res = await getChannelAPI()
+      setChannelList(res.data.channels)
+    }
+    // 2. 调用函数
+    getChannelList()
+  }, [])
   return (
     <div className="publish">
       <Card
@@ -46,7 +59,13 @@ const Publish = () => {
             rules={[{ required: true, message: '请选择文章频道' }]}
           >
             <Select placeholder="请选择文章频道" style={{ width: 400 }}>
-              <Option value={0}>推荐</Option>
+              {
+                channelList.map(item => {
+                  return (
+                    <Option key={item.id} value={item.id}>{item.name}</Option>
+                  )
+                })
+              }
             </Select>
           </Form.Item>
           <Form.Item
