f2fd8e64fa47345e6784e46a7efaad53b14ab2df
duncaiyun <759830766@qq.com>
2024-08-29 16:59:51 +0800

    发布文章-基础文章发布

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -15,7 +15,7 @@ import './index.scss'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import { useEffect, useState } from 'react'
-import { getChannelAPI } from '@/apis/article'
+import { createArticleAPI, getChannelAPI } from '@/apis/article'
 const { Option } = Select
 
 const Publish = () => {
@@ -30,6 +30,24 @@ const Publish = () => {
     // 2. 调用函数
     getChannelList()
   }, [])
+
+  // 提交表单
+  const onFinish = (formValue) => {
+    // 1、按照接口文档的格式处理收集到的表单数据
+    const { channel_id, content, title } = formValue
+
+    const reqData = {
+      channel_id,
+      content,
+      title,
+      cover: {
+        type: 1,
+        images: []
+      }
+    }
+    // 2、调用接口
+    createArticleAPI(reqData)
+  }
   return (
     <div className="publish">
       <Card
@@ -45,6 +63,7 @@ const Publish = () => {
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
           initialValues={{ type: 1 }}
+          onFinish = {onFinish}
         >
           <Form.Item
             label="标题"
