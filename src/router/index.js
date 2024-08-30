b5efe3b1f70912f4b05de4b05d663738f3232797
duncaiyun <759830766@qq.com>
2024-08-30 14:10:53 +0800

    打包路由懒加载优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 // 路由配置
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
-import Publish from '@/pages/Publish'
-import Article from '@/pages/Article'
-import Home from '@/pages/Home'
 import AuthRoute from '@/components/AuthRoute'
+import {createBrowserRouter} from "react-router-dom";
+import { Suspense, lazy } from 'react'
 
+// 1.lazy函数对组件进行导入
+
+const Home = lazy(() => import('@/pages/Home'))
+const Article = lazy(() => import('@/pages/Article'))
+const Publish = lazy(() => import('@/pages/Publish'))
 
 // 配置路由实例
-import {createBrowserRouter} from "react-router-dom";
+
   
   const router = createBrowserRouter([
     {
@@ -16,16 +20,16 @@ import {createBrowserRouter} from "react-router-dom";
       element: <AuthRoute><Layout/></AuthRoute>,
       children:[
         {
-          index: 1,
-          element: <Home/>,
+          index: true,
+          element: <Suspense fallback={'加载中'}><Home/></Suspense>,
         },
         {
           path: "article",
-          element: <Article/>,
+          element: <Suspense fallback={'加载中'}><Article/></Suspense>,
         },
         {
           path: "publish",
-          element: <Publish/>,
+          element: <Suspense fallback={'加载中'}><Publish/></Suspense>,
         }
       ]
     },
